1b2a87a0a595974d5f1cbafd2dd609dfdc058e7c
Amit Patel <amitpatel_7@msn.com>
2016-04-07 12:23:39 -0700

    "split map and viewmodel into separate files"
    I split up the map function (along with its marker functions) into its own file and linked to it in my html file

--- js/main.js
+++ js/main.js
@@ -31,50 +31,7 @@ var LocationData = [
 
 /////*VIEWMODEL*/////
 
-//Renders Map on screen
-function initialize()
-{
-    var map =
-        new google.maps.Map(document.getElementById('map-canvas'));
-    var bounds = new google.maps.LatLngBounds();
-    var infowindow = new google.maps.InfoWindow();
 
-//A forEach fucntion that uses LocationData to generate multiple markers on the map
-LocationData.forEach(function(location)) {
-    new google.maps(location.title, location.lat, location.lng);
-
-        var marker = new google.maps.Marker({
-            map: map,
-            draggable: true,
-            animation: google.maps.Animation.DROP,
-            position: "lat" + "," + "lng",
-            title: p[0]
-        });
-        marker.addListener('click', toggleBounce);
-//Bounce animation for when the user clicks on a marker
-function toggleBounce() {
-  var self = this;
-  if (this.getAnimation() !== null) {
-    this.setAnimation(null);
-  } else {
-        this.setAnimation(google.maps.Animation.BOUNCE);
-        setTimeout(function() {
-            self.setAnimation(null)
-        }, 1000);
-    }
-};
-
-//An event listener to display an info window when the user clicks on a marker
-        google.maps.event.addListener(marker, 'click', function() {
-            infowindow.setContent(this.title);
-            infowindow.open(map, this);
-        });
-    }
-}
-    map.fitBounds(bounds);
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
 
 //function appViewModel() {
 //    var self = this;
