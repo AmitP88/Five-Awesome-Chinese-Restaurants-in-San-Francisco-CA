d4ffc48e762996547a47e6cd60c8bc8f02d1c8d2
Amit Patel <amitpatel_7@msn.com>
2016-04-07 17:36:13 -0700

    "removed data model and placed into maps.js"

--- js/main.js
+++ js/main.js
@@ -1,37 +1,4 @@
-/////*MODEL*/////
-
-//Locations for the markers on the map
-var LocationData = [
-	{
-    "title": "Capital Restaurant",
-    "lat": 37.793945,
-    "lng": -122.407079
-    },
-    {
-    "title": "Shangri-Li Chinese Vegetarian",
-    "lat": 37.763695,
-    "lng": -122.479830
-    },
-    {
-    "title": "Riverside Seafood Restaurant",
-    "lat": 37.738948,
-    "lng": -122.479902
-    },
-    {
-    "title": "City View Restaurant",
-    "lat": 37.794188,
-    "lng": -122.404117
-    },
-    {
-    "title": "Five Happiness",
-    "lat": 37.781281,
-    "lng": -122.463974
-    }
-];
-
 /////*VIEWMODEL*/////
-
-
 
 //function appViewModel() {
 //    var self = this;
