fd6544ba075974c3bee4b6855dab5b18614dc258
Amit Patel <amitpatel_7@msn.com>
2016-05-02 18:13:38 -0700

    added functionality for listview and markers
    Added ViewModel, restaurantLocation, and self.listViewClick functions for listview functionality (with bindings for markers)

--- js/map.js
+++ js/map.js
@@ -1,84 +1,105 @@
-/////*VIEWMODEL*/////
-
-/*Class to represent a row in the Places grid*/
-function Place (name, initialLocation) {
-    var self = this;
-    self.name = name;
-    self.location = ko.observable(initialLocation);
-
-
-
-};
-
 /////*MODEL*/////
-
-/*Overall viewmodel for the listview with initial statements*/
-function MapViewModel() {
-    var self = this;
-
-/*Non-editable data*/
-self.locations = [
-	{ name: "Capital Restaurant", position: {lat: 37.793945, lng: -122.407079}},
-    { name: "Shangri-Li Chinese Vegetarian", position: {lat: 37.763695, lng: -122.479830}},
-    { name: "Riverside Seafood Restaurant", position: {lat: 37.738948, lng: -122.479902}},
-    { name: "City View Restaurant", position: {lat: 37.794188, lng: -122.404117}},
-    { name: "Five Happiness", position: {lat: 37.781281, lng: -122.463974}}
+var initialLocations = [
+    {name: "Capital Restaurant", position: {lat: 37.793945, lng: -122.407079}},
+    {name: "Shangri-Li Chinese Vegetarian", position: {lat: 37.763695, lng: -122.479830}},
+    {name: "Riverside Seafood Restaurant", position: {lat: 37.738948, lng: -122.479902}},
+    {name: "City View Restaurant", position: {lat: 37.794188, lng: -122.404117}},
+    {name: "Five Happiness", position: {lat: 37.781281, lng: -122.463974}}
 ];
 
-/*Editable data*/
-self.places = ko.observableArray([
-    new Place("Capital Restaurant", self.locations[1]),
-    new Place("Shangri-Li Chinese Vegetarian", self.locations[1]),
-    new Place("Riverside Seafood Restaurant", self.locations[1]),
-    new Place("City View Restaurant", self.locations[1]),
-    new Place("Five Happiness", self.locations[1]),
-    ]);
+//Variable for infowindow content
+var content = "(Insert Yelp! API info here)";
 
-}
+//Declared map, marker, and infoWindow variables to be used later downstream
 
-ko.applyBindings(new MapViewModel());
+var map;
 
-////*MAP*////
+var infoWindow;
 
-//Renders Map on screen
-function initMap()
-{
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 37.7749, lng: -122.4194},
-        zoom: 12
-    });
+//Function that renders the map on screen using the Id "map" as a reference
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: {lat: 37.7749, lng: -122.4194},
+    zoom: 12,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
+};
 
-//A forEach fucntion that uses LocationData to generate multiple markers on the map
+/////*VIEWMODEL*/////
+function ViewModel() {
 
-locations.forEach(function(locations) {
-    var marker = new google.maps.Marker({
-            map: map,
-            draggable: true,
-            animation: google.maps.Animation.DROP,
-            position: locations.position,
-            title: locations.name,
-        });
-    location.markerData = marker;
-        marker.addListener('click', toggleBounce);
-    });
+  // tells the view model what to do when a change occurs
+  function restaurantLocation(name, position) {
 
-//Bounce animation for when the user clicks on a marker
-function toggleBounce() {
+  var marker;
+
+  this.name = ko.observable(value.name);
+  this.position = ko.observable(value.lat);
+  };
+
   var self = this;
-  if (this.getAnimation() !== null) {
-    this.setAnimation(null);
-  } else {
-        this.setAnimation(google.maps.Animation.BOUNCE);
-        setTimeout(function() {
-            self.setAnimation(null)
-                    }, 1000);
+  self.markers = [];
+
+  //Copies the values of initialLocations and stores them in sortedLocations(); observableArray
+  self.sortedLocations = ko.observableArray(initialLocations);
+
+  //Adds new markers at each location in the initialLocations Array
+  self.sortedLocations().forEach(function(location) {
+    marker = new google.maps.Marker({
+      position: location.position,
+      map: map,
+      title: location.name,
+      icon: 'http://icons.iconarchive.com/icons/pixture/box-container/32/Chinese-icon.png',
+      animation: google.maps.Animation.DROP
+    });
+
+    location.marker = marker;
+
+  //Pushes each marker into the markers array
+    this.markers.push(marker);
+  });
+
+  //Map info windows to each item in the markers array
+  self.markers.map(function(info) {
+    infoWindow = new google.maps.InfoWindow({
+      content: content
+    });
+    //Add click event to each marker to open info window
+    info.addListener('click', function() {
+      infoWindow.open(map, this),
+        info.setAnimation(google.maps.Animation.BOUNCE) //Markers will bounce when clicked
+      setTimeout(function() {
+        info.setAnimation(null)
+      }, 2000); //Change value to null after 2 seconds and stop markers from bouncing
+    });
+
+  });
+
+  //Click on item in list view
+  self.listViewClick = function(restaurant) {
+    if (restaurant.name) {
+      map.setZoom(15); //Zoom map view
+      map.panTo(restaurant.position); // Pan to correct marker when list view item is clicked
+      restaurant.marker.setAnimation(google.maps.Animation.BOUNCE); // Bounce marker when list view item is clicked
+      infoWindow.open(map, restaurant.marker); // Open info window on correct marker when list item is clicked
     }
+    setTimeout(function() {
+      restaurant.marker.setAnimation(null); // End animation on marker after 2 seconds
+    }, 2000);
+  };
+
+  // Stores user input
+  self.query = ko.observable('');
+
+  //Filter through observableArray and filter results using knockouts utils.arrayFilter();
+  self.search = ko.computed(function() {
+    return ko.utils.arrayFilter(self.sortedLocations(), function(listResult) {
+      return listResult.name.toLowerCase().indexOf(self.query().toLowerCase()) >= 0;
+    });
+  });
 };
 
-   //An event listener to display an info window when the user clicks on a marker
-        google.maps.event.addListener(marker, 'click', function() {
-            infowindow.setContent(this.title);
-            infowindow.open(map, this);
-        });
-    map.fitBounds(bounds);
-};
+$(document).ready(function() {
+  initMap();
+  ko.applyBindings(ViewModel());
+});
